b25d2fbdd53e840237638066ed624fc3f25619d9
ameera-aynahealth <ameera@aynahealth.co>
2026-09-02 21:44:44 -0400

    Trace only exact beta replacement seed

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   outputFileTracingIncludes: {
-    "/projects/**": [
-      "./db/migrations/0001_best_version_master_list.sql",
-      "./db/migrations/0002_beta_phase_labels.sql",
-      "./db/bootstrap/best_version_scope_cleanup.sql",
-    ],
+    "/projects/**": ["./db/bootstrap/best_version_scope_cleanup.sql"],
   },
 };
 
