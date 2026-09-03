94c896bd90cf6a846ac2be0981bd70f88bea3162
ameera-aynahealth <ameera@aynahealth.co>
2026-09-02 21:33:45 -0400

    Trace Best Version scope cleanup SQL

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
     "/projects/**": [
       "./db/migrations/0001_best_version_master_list.sql",
       "./db/migrations/0002_beta_phase_labels.sql",
+      "./db/bootstrap/best_version_scope_cleanup.sql",
     ],
   },
 };
