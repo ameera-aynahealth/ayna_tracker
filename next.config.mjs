cf7c6fe1aa3823c2f90c39c89a38321c9bc6165b
ameera-aynahealth <ameera@aynahealth.co>
2026-09-02 22:02:04 -0400

    Trace health intake beta requirements

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   outputFileTracingIncludes: {
-    "/projects/**": ["./db/bootstrap/best_version_scope_cleanup.sql"],
+    "/projects/**": [
+      "./db/bootstrap/best_version_scope_cleanup.sql",
+      "./db/bootstrap/best_version_health_intake_details.sql",
+    ],
   },
 };
 
